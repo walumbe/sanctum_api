15eac1a72edeeed30804b0dd48064f3c3e508bd1
walumbe <nathanwalumbe@gmail.com>
2023-04-15 23:46:32 +0300

    Store task

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -23,27 +23,27 @@ class TasksController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        $request->validated();
+        $task = Task::create([
+            'user_id' => Auh::user()->id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'priority' => $request->priority
+        ]);
+
+        return new TasksResource($task);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
+        return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task): new TasksResource($task);
     }
 
     /**
